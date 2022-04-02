93333bde408c1a4ca901882eac33c506bf697d16
Lazy Phisher <34194184+Mr219@users.noreply.github.com>
2022-04-02 16:51:14 +0630

    Update main.go

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main(){
 	   			if check_result != false {
 	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
 	   			}else{
-	   				fmt.Println(string(colorGreen),"N.", string(colorReset))
+	   				fmt.Println(string(colorGreen),"\t.", string(colorReset))
 	   			}
 
 			}

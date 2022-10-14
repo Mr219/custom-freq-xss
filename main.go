0f30034428670178bac0035804734e98612d6978
Lazy Phisher <34194184+Mr219@users.noreply.github.com>
2022-10-14 13:19:18 +0630

    Update main.go

--- main.go
+++ main.go
@@ -17,54 +17,5 @@ func main(){
 	fmt.Println("\\__(-_-)__/")
 	// fmt.Println("\n")
 
-	colorReset := "\033[0m"
-	colorRed := "\033[31m"
-    //colorGreen := "\033[32m"
 
-
-	sc := bufio.NewScanner(os.Stdin)
-
-	jobs := make(chan string)
-	var wg sync.WaitGroup
-
-	for i:= 0; i < 20; i++{
-
-		wg.Add(1)
-		go func(){
-			defer wg.Done()
-			for domain := range jobs {
-
-				resp, err := http.Get(domain)
-				if err != nil{
-					continue
-				}
-				body, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-	      			fmt.Println(err)
-	   			}
-	   			sb := string(body)
-	   			check_result := strings.Contains(sb , "Minxss\"'")
-	   			// fmt.Println(check_result)
-	   			if check_result != false {
-	   				fmt.Println(string(colorRed),"\nPossible To XSS:", domain,string(colorReset))
-	   			}else{
-					fmt.Printf(".")
-	   			}
-
-			}
-			
-   		}()
-
-	}
-
-
-
-	for sc.Scan(){
-		domain := sc.Text()
-		jobs <- domain		
-		
-
-	}
-	close(jobs)
-	wg.Wait()
 }

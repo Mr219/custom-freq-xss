7fb67d3b11771af5868120d4c092d540822f0049
Lazy Phisher <34194184+Mr219@users.noreply.github.com>
2022-10-14 13:22:23 +0630

    Update main.go

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 func main(){
 	// fmt.Println("\n")
 	fmt.Println("qsreplace Minxss'\" !!")
-	fmt.Println(os.Args[0]+" "+os.Args[1])
-	fmt.Println("\\__(-_-)__/")
+	fmt.Println(os.Args[0]os.Args[1])
+	fmt.Println("__________________")
 	// fmt.Println("\n")
 
 
